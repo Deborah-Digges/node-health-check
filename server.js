36b7734e7bf5ae09e37271f6496129976cad6b6c
Deborah Gertrude Digges <DeborahGertrude_Digges@intuit.com>
2015-10-13 21:39:42 +0530

    Add configuration for port numebr

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var statusController = require('./controllers/statuses');
-var pushInterval = 10000; 
+var pushInterval = 10000;
+var port = process.env.PORT || 3000;
+
 
 app.use(bodyParser());
 require('./config/routes')(app);
-app.set('port', 3000);
+app.set('port', port);
 app.use(require('./controllers/static'));
 
 io.on('connection', function(socket){
